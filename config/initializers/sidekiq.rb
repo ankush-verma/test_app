3efdcca791dae7fcf123707071971a04d2a1e7ec
ror <dev@gmail.com>
2017-09-24 18:48:59 +0530

    sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,9 +6,9 @@ Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ["test", "test"]
 end
  Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis:// 127.0.0.1:6379' }
+  config.redis = { url: '127.0.0.1:6379/1' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379' }
+  config.redis = { url: '127.0.0.1:6379/1' }
 end
